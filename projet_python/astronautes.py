0daf43c621d9d8e45ad0251f4ebcfc9f8dfe7590
Laurent Ughetto <laurent.ughetto@univ-rennes2.fr>
2022-03-01 15:09:19 +0100

    méthode alternative récup txt

--- projet_python/astronautes.py
+++ projet_python/astronautes.py
@@ -1,7 +1,13 @@
 import requests
 
+# récup des infos sur le web
 url = "http://api.open-notify.org/astros.json"
-
 reponse = requests.get(url)
+
+#
 contenu = reponse.json()
 print(contenu)
+
+# alternative
+texte = reponse.text
+print(texte)
